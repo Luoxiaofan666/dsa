67ede6c2cf91ac9e8c97c6ecde8f804074fdc48a
蓝风筝 <1310657438@qq.com>
2019-10-03 15:05:30 +0800

    栈结构的测试

--- queen_stack/main.cpp
+++ queen_stack/main.cpp
@@ -21,9 +21,10 @@ int nCheck = 0; //尝试的总次数
  * n皇后（迭代版）
  ******************************************************************************************/
 int main ( int argc, char* argv[] ) {
-   for ( int i = 1; i < argc - 1; i++ ) //检查参数
-      if ( !strcmp ( "-step", argv[i] ) ) runMode = Step; //设定为单步演示模式
-   int nQueen = atoi ( argv[argc-1] ); //棋盘大小 = 皇后总数
+   //for ( int i = 1; i < argc - 1; i++ ) //检查参数
+   //   if ( !strcmp ( "-step", argv[i] ) ) 
+		  runMode = Step; //设定为单步演示模式
+   int nQueen = 5; //棋盘大小 = 皇后总数
    if ( QUEEN_MAX < nQueen || 1 > nQueen ) { //检查参数
       fprintf ( stderr, "\nUsage: %s [-step] <nQueen(%d)>\n", argv[0], QUEEN_MAX );
       return -1;
